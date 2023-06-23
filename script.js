7670ea4173e9d72a2541b0eae656550f83cc7574
Ana <anavitoria.goes@hotmail.com>
2023-06-23 11:04:00 -0300

    moon effect light-dark

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let modeSwitch = document.querySelector(".mode-switch");
+
+  modeSwitch.addEventListener("click", function () {
+    document.documentElement.classList.toggle("dark");
+    modeSwitch.classList.toggle("active");
+  });
+});
